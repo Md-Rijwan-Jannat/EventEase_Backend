0a4a9213853bf129066d9c09f50190c2221cb788
Md Rijwan Jannat <rijwanjannat36@gmail.com>
2025-01-12 11:44:56 +0600

    Setup a Node, Express, Mongoose(ODM), MongoDB Backend application

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export default {
   port: process.env.PORT,
-  database_url: process.env.database_url,
+  database_url: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
+  bcrypt_salt_rounds: process.env.BCRYPT_SLAT_ROUNDS,
   cloudinary_cloud_name: process.env.cloudinary_cloud_name,
   cloudinary_api_key: process.env.cloudinary_api_key,
   cloudinary_api_secret: process.env.cloudinary_api_secret,
-  bcrypt_salt_rounds: process.env.bcrypt_salt_rounds,
 };
